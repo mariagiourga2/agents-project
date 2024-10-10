246b2f11ca4047ff4db1eec24ab4973a70ae0dc2
mariagiourga2 <mariagiourga2000@gmail.com>
2024-10-10 13:25:01 +0300

    city_description.text

--- Assets/Scripts/CityMap.cs
+++ Assets/Scripts/CityMap.cs
@@ -1,37 +1,23 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
 public class CityMap : MonoBehaviour
 {
-    // Ρυθμίσεις για τον χάρτη
-    public int N = 300;  // Ύψος του χάρτη
-    public int M = 260;  // Πλάτος του χάρτη
-
     // Path για το αρχείο εξαγωγής
     private string filePath;
 
     void Start()
     {
-        filePath = Application.dataPath + "/city_map.txt";  // Ορισμός διαδρομής αρχείου
+        filePath = Application.dataPath + "/city_description.txt";  // Ορισμός διαδρομής αρχείου
         ExportCityToFile();
     }
 
     void ExportCityToFile()
     {
-        // Δημιουργία πίνακα για την αναπαράσταση του χάρτη
-        char[,] cityMap = new char[N, M];
-
-        // Αρχικοποίηση με κενές θέσεις
-        for (int i = 0; i < N; i++)
-        {
-            for (int j = 0; j < M; j++)
-            {
-                cityMap[i, j] = ' ';  // Κενή θέση
-            }
-        }
+        // Δημιουργία λίστας για την περιγραφή του χάρτη
+        List<string> cityDescription = new List<string>();
 
         // Εύρεση αντικειμένων στον κόσμο μέσω tags
         GameObject[] buildings = GameObject.FindGameObjectsWithTag("Building");
@@ -41,100 +27,70 @@ public class CityMap : MonoBehaviour
         GameObject[] props = GameObject.FindGameObjectsWithTag("Props");
         GameObject[] cars = GameObject.FindGameObjectsWithTag("Cars");
 
-        // Ενημέρωση πίνακα με βάση τη θέση και τον τύπο αντικειμένου
+        // Καταγραφή των ασήμαντων κτιρίων
         foreach (GameObject building in buildings)
         {
             Vector3 pos = building.transform.position;
-            int x = Mathf.FloorToInt(pos.z);
-            int y = Mathf.FloorToInt(pos.x);
-
-            if (x >= 0 && x < N && y >= 0 && y < M)
-            {
-                cityMap[x, y] = '*';  // Ασήμαντα κτίρια
-            }
+            string description = $"Building at position (x: {pos.x}, y: {pos.y}, z: {pos.z}) marked as '*'";
+            cityDescription.Add(description);
         }
 
+        // Καταγραφή των σπιτιών πρακτόρων
         foreach (GameObject home in agentHomes)
         {
             Vector3 pos = home.transform.position;
-            int x = Mathf.FloorToInt(pos.z);
-            int y = Mathf.FloorToInt(pos.x);
-
-            if (x >= 0 && x < N && y >= 0 && y < M)
-            {
-                // Χρήση ψηφίων 0-9 για τα σπίτια πρακτόρων
-                int digit = UnityEngine.Random.Range(0, 10);  // Δημιουργία τυχαίου ψηφίου
-                cityMap[x, y] = (char)('0' + digit);
-            }
+            int digit = UnityEngine.Random.Range(0, 10);  // Χρήση ψηφίων 0-9
+            string description = $"AgentHome at position (x: {pos.x}, y: {pos.y}, z: {pos.z}) marked as '{digit}'";
+            cityDescription.Add(description);
         }
 
+        // Καταγραφή των δρόμων
         foreach (GameObject road in roads)
         {
             Vector3 pos = road.transform.position;
-            int x = Mathf.FloorToInt(pos.z);
-            int y = Mathf.FloorToInt(pos.x);
-
-            if (x >= 0 && x < N && y >= 0 && y < M)
-            {
-                cityMap[x, y] = 'R';  // Δρόμοι
-            }
+            string description = $"Road at position (x: {pos.x}, y: {pos.y}, z: {pos.z}) marked as 'R'";
+            cityDescription.Add(description);
         }
 
+        // Καταγραφή των πάρκων
         foreach (GameObject park in parks)
         {
             Vector3 pos = park.transform.position;
-            int x = Mathf.FloorToInt(pos.z);
-            int y = Mathf.FloorToInt(pos.x);
-
-            if (x >= 0 && x < N && y >= 0 && y < M)
-            {
-                cityMap[x, y] = 'P';  // Πάρκα
-            }
+            string description = $"Park at position (x: {pos.x}, y: {pos.y}, z: {pos.z}) marked as 'P'";
+            cityDescription.Add(description);
         }
 
+        // Καταγραφή των props
         foreach (GameObject prop in props)
         {
             Vector3 pos = prop.transform.position;
-            int x = Mathf.FloorToInt(pos.z);
-            int y = Mathf.FloorToInt(pos.x);
-
-            if (x >= 0 && x < N && y >= 0 && y < M)
-            {
-                cityMap[x, y] = 'X';  // Props
-            }
+            string description = $"Prop at position (x: {pos.x}, y: {pos.y}, z: {pos.z}) marked as 'X'";
+            cityDescription.Add(description);
         }
 
+        // Καταγραφή των αυτοκινήτων
         foreach (GameObject car in cars)
         {
             Vector3 pos = car.transform.position;
-            int x = Mathf.FloorToInt(pos.z);
-            int y = Mathf.FloorToInt(pos.x);
-
-            if (x >= 0 && x < N && y >= 0 && y < M)
-            {
-                cityMap[x, y] = 'C';  // Αυτοκίνητα
-            }
+            string description = $"Car at position (x: {pos.x}, y: {pos.y}, z: {pos.z}) marked as 'C'";
+            cityDescription.Add(description);
         }
 
-        // Εξαγωγή του πίνακα σε αρχείο κειμένου
+        // Εξαγωγή της περιγραφής σε αρχείο κειμένου
         try
         {
             using (StreamWriter writer = new StreamWriter(filePath, false, System.Text.Encoding.UTF8))
             {
-                for (int i = 0; i < N; i++)
+                foreach (string line in cityDescription)
                 {
-                    for (int j = 0; j < M; j++)
-                    {
-                        writer.Write(cityMap[i, j]);
-                    }
-                    writer.WriteLine();  // Νέα γραμμή για κάθε σειρά
+                    writer.WriteLine(line);
                 }
             }
-            Debug.Log("City map successfully exported to " + filePath);
+            Debug.Log("City description successfully exported to " + filePath);
         }
         catch (Exception e)
         {
-            Debug.LogError("Failed to export city map: " + e.Message);
+            Debug.LogError("Failed to export city description: " + e.Message);
         }
     }
 }
