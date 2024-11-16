ec8ff8244f423f9ef74e76d808489e10edb06285
mariagiourga2 <mariagiourga2000@gmail.com>
2024-11-16 19:24:47 +0200

    health

---
+++ Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Rendering;
+
+public class InventoryUI : MonoBehaviour
+{
+    private TextMeshProUGUI diamondText;
+    void Start()
+    {
+        diamondText = GetComponent<TextMeshProUGUI>();
+    }
+    public void UpdateDiamondText(PlayerInventory playerInventory)
+    {
+        diamondText.text = playerInventory.NumberOfDiamonds.ToString();
+    }
+}
